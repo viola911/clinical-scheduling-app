bbd97ba1aa9c8650cbe35ebc420be7e969016f67
omarhilall <102355844+omarhilall@users.noreply.github.com>
2022-10-27 23:11:17 +0200

    omarhilal's part

---
+++ editprofileUSER.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp1());
+}
+
+class MyApp1 extends StatelessWidget {
+  const MyApp1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'MindKhan.com',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Define a key to access the form
+  final _formKey = GlobalKey<FormState>();
+
+  String _userName = '';
+  String _password = '';
+  String _confirmPassword = '';
+
+  // This function is triggered when the user press the "Sign Up" button
+  void _trySubmitForm() {
+    final bool? isValid = _formKey.currentState?.validate();
+    if (isValid == true) {
+      debugPrint('Everything looks good!');
+      debugPrint(_userName);
+      debugPrint(_password);
+      debugPrint(_confirmPassword);
+
+      /* 
+      Continute proccessing the provided information with your own logic 
+      such us sending HTTP requests, savaing to SQLite database, etc.
+      */
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.lightBlue,
+        alignment: Alignment.center,
+        child: Center(
+          child: Card(
+            margin: const EdgeInsets.symmetric(horizontal: 35),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      /// username
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Username'),
+                        validator: (value) {
+                          if (value == null || value.trim().isEmpty) {
+                            return 'This field is required';
+                          }
+                          if (value.trim().length < 4) {
+                            return 'Username must be at least 4 characters in length';
+                          }
+                          // Return null if the entered username is valid
+                          return null;
+                        },
+                        onChanged: (value) => _userName = value,
+                      ),
+
+                      /// Password
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Password'),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.trim().isEmpty) {
+                            return 'This field is required';
+                          }
+                          if (value.trim().length < 8) {
+                            return 'Password must be at least 8 characters in length';
+                          }
+                          // Return null if the entered password is valid
+                          return null;
+                        },
+                        onChanged: (value) => _password = value,
+                      ),
+
+                      /// Confirm Password
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is required';
+                          }
+
+                          if (value != _password) {
+                            return 'Confimation password does not match the entered password';
+                          }
+
+                          return null;
+                        },
+                        onChanged: (value) => _confirmPassword = value,
+                      ),
+                      const SizedBox(height: 20),
+                      Container(
+                          alignment: Alignment.centerRight,
+                          child: OutlinedButton(
+                              onPressed: _trySubmitForm,
+                              child: const Text('Save Changes')))
+                    ],
+                  )),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
