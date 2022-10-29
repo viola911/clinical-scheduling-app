5efdfdd790bb31cdc6db30343c25776a588f1baa
viola911 <84061125+viola911@users.noreply.github.com>
2022-10-29 05:17:21 -0200

    Add files via upload

--- userHome.dart
+++ userHome.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:scheduling_app/Schedules.dart';
+import 'package:scheduling_app/YourAppointments.dart';
 import 'package:scheduling_app/constants.dart';
-import 'package:scheduling_app/editprofieAdmin.dart';
+import 'package:scheduling_app/editprofileUSER.dart';
 import 'package:scheduling_app/model/model.dart';
-import 'package:scheduling_app/model/posts.dart';
+
 import 'package:scheduling_app/writePost.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import 'callscreen.dart';
 
-class Homescreen2 extends StatefulWidget {
-  const Homescreen2({super.key});
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
 
   @override
-  State<Homescreen2> createState() => _Homescreen2State();
+  State<Homescreen> createState() => _HomescreenState();
 }
 
-class _Homescreen2State extends State<Homescreen2> {
+class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,17 +52,27 @@ class _Homescreen2State extends State<Homescreen2> {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const Homescreen2()));
+                        builder: (context) => const Homescreen()));
               },
             ),
             ListTile(
               leading: const Icon(
                 Icons.post_add,
               ),
-              title: const Text('Write Post'),
+              title: const Text('Schedule Appointment'),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => sched()));
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.post_add,
+              ),
+              title: const Text('Your Appointment'),
               onTap: () {
                 Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const Post()));
+                    MaterialPageRoute(builder: (context) => LoadMoreWidget()));
               },
             ),
             ListTile(
@@ -70,7 +82,7 @@ class _Homescreen2State extends State<Homescreen2> {
               title: const Text('Edit Profile'),
               onTap: () {
                 Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const HomePage()));
+                    MaterialPageRoute(builder: (context) => Editprof()));
               },
             ),
             ListTile(
@@ -138,7 +150,7 @@ class _Homescreen2State extends State<Homescreen2> {
                     Row(
                       children: const [
                         Text(
-                          "Dr.Hadyi",
+                          "Marie",
                           style:
                               TextStyle(fontSize: 35, color: kSecondaryColor),
                         ),
@@ -147,116 +159,9 @@ class _Homescreen2State extends State<Homescreen2> {
                   ],
                 ),
               ),
-              //data section
-              TableCalendar(
-                focusedDay: DateTime.now(),
-                firstDay: DateTime(2022),
-                lastDay: DateTime(2023),
-                calendarFormat: CalendarFormat.week,
-                daysOfWeekVisible: true,
-                headerVisible: true,
-                calendarStyle: CalendarStyle(
-                  weekendDecoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: kPrimaryColor),
-                  ),
-                  defaultDecoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: kPrimaryColor),
-                  ),
-                  todayDecoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: kSecondaryColor),
-                  ),
-                  todayTextStyle:
-                      const TextStyle(fontSize: 20, fontFamily: 'Oswald'),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: products.length,
-                  itemBuilder: (context, index) {
-                    var patientsCard = PatientsCard(
-                      product: products[index],
-                      press: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    Callscreen(product: products[index])));
-                      },
-                    );
-                    return patientsCard;
-                  },
-                ),
-              ),
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class PatientsCard extends StatelessWidget {
-  const PatientsCard({
-    Key? key,
-    required this.product,
-    required this.press,
-  }) : super(key: key);
-  final Product product;
-  final void Function() press;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Row(
-        children: [
-          InkWell(
-            onTap: press,
-            child: Row(
-              children: [
-                Text(product.time),
-              ],
-            ),
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: SizedBox(
-                      height: 60,
-                      width: 60,
-                      child: Image.asset(product.image, fit: BoxFit.cover)),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      product.name,
-                      style: const TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black38,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Icon(product.icon)
-        ],
       ),
     );
   }
---
+++ writePost.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:scheduling_app/callscreen.dart';
+import 'package:scheduling_app/constants.dart';
+import 'package:scheduling_app/homescreen.dart';
+import 'package:scheduling_app/model/model.dart';
+import 'package:scheduling_app/model/posts.dart';
+
+class Post extends StatefulWidget {
+  const Post({Key? key}) : super(key: key);
+
+  @override
+  State<Post> createState() => _PostState();
+}
+
+class _PostState extends State<Post> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Mind Khan',
+            style: TextStyle(fontSize: 30),
+          ),
+          backgroundColor: kSecondaryColor),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration:
+                  BoxDecoration(color: Color.fromARGB(255, 8, 100, 118)),
+              child: Text(
+                'Options',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.edit,
+              ),
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Homescreen2()));
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.edit,
+              ),
+              title: const Text('Write Post'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.train,
+              ),
+              title: const Text('Edit Profile'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
