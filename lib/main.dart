3cccfaf0add9660955fb6042d2fa0e471b61f314
omarhilall <102355844+omarhilall@users.noreply.github.com>
2023-01-16 13:33:24 +0200

    fixing main

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,14 @@ import 'package:scheduling_app/screens/Schedules.dart';
 import 'package:scheduling_app/screens/YourAppointments.dart';
 import 'package:scheduling_app/screens/editprofileUSER.dart';
 import 'package:scheduling_app/screens/intro.dart';
-import 'package:scheduling_app/screens/logch.dart';
-import 'package:scheduling_app/services/routes.dart';
 import 'package:go_router/go_router.dart';
+import 'package:scheduling_app/screens/signinpageUSER.dart';
+import 'package:scheduling_app/screens/signuppageUSER.dart';
 
 import 'screens/Payment.dart';
 
 import 'screens/editprofieAdmin.dart';
 import 'screens/homescreen.dart';
-import 'model/posts.dart';
-import 'services/routes.dart';
 import 'screens/signinpageADMIN.dart';
 import 'screens/userHome.dart';
 import 'screens/writePost.dart';
@@ -75,15 +73,16 @@ final GoRouter _route = GoRouter(routes: <GoRoute>[
       GoRoute(
         path: 'AdminScreen',
         builder: (BuildContext context, GoRouterState state) =>
-            HomescreenAdmin(),
+            const HomescreenAdmin(),
       ),
       GoRoute(
         path: 'Payment',
-        builder: (BuildContext context, GoRouterState state) => Payment(),
+        builder: (BuildContext context, GoRouterState state) => const Payment(),
       ),
       GoRoute(
         path: 'SignInAdmin',
-        builder: (BuildContext context, GoRouterState state) => SignInAdmin(),
+        builder: (BuildContext context, GoRouterState state) =>
+            const signinAdmin(),
       ),
       GoRoute(
         path: 'Appointments',
@@ -92,8 +91,10 @@ final GoRouter _route = GoRouter(routes: <GoRoute>[
       ),
       GoRoute(
         path: "post",
-        builder: (BuildContext context, GoRouterState state) => Post(),
+        builder: (BuildContext context, GoRouterState state) => const Post(),
       ),
+      GoRoute(path: 'signinUser', builder: (context, state) => const Signin()),
+      GoRoute(path: 'signup', builder: (context, state) => const SignUp()),
     ],
     path: '/',
     builder: (BuildContext context, GoRouterState state) => const IntroScreen(),
