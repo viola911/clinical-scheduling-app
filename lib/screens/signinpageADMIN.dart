a82f1b7c1b2a7a656c512a3ffc9f4a78767fb79b
omarhilall <102355844+omarhilall@users.noreply.github.com>
2023-01-16 13:27:03 +0200

    fixing signin(Admin)

--- lib/screens/signinpageADMIN.dart
+++ lib/screens/signinpageADMIN.dart
@@ -1,74 +1,45 @@
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 import '../services/constants.dart';
+import '../screens/homescreen.dart';
 
-// Create a Form widget.
-class SignInAdmin extends StatefulWidget {
-  const SignInAdmin({super.key});
+const Color black = Color(0xFF000000);
 
-  @override
-  SignInAdminState createState() {
-    return SignInAdminState();
-  }
-}
+class signinAdmin extends StatelessWidget {
+  const signinAdmin({super.key});
 
-class SignInAdminState extends State<SignInAdmin> {
-  final _formKey = GlobalKey<FormState>();
-  TextEditingController IdController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Welcome "),
+          centerTitle: true,
+          backgroundColor: kSecondaryColor),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(10),
-              child: const Text(
-                'Enter Your ID!',
-                style: TextStyle(fontSize: 20),
-              )),
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: Icon(Icons.person),
-              contentPadding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+          const Text('Please Enter you ID!'),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 8),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: ' ID',
+              ),
             ),
-            // The validator receives the text that the user has entered
-            validator: (value) {
-              return (value != null && value.contains('1234'))
-                  ? 'Signed IN.'
-                  : 'Enter a valid ID!';
-            },
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              style: TextButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  elevation: 2,
-                  backgroundColor: kPrimaryColor),
-              onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
-                if (_formKey.currentState!.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Processing Data')),
-                  );
-                }
-              },
-              child: const Text('Submit'),
-            ),
+          const SizedBox(
+            height: 30,
           ),
           TextButton(
+            style: TextButton.styleFrom(
+                foregroundColor: Colors.black,
+                elevation: 2,
+                backgroundColor: kPrimaryColor),
             onPressed: () {
-              //forgot ID screen
+              context.go('/AdminScreen');
             },
-            child: const Text(
-              'Forgot ID',
-            ),
+            child: const Text('Sign In'),
           ),
         ],
       ),
